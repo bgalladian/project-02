24938fbbe18b743add84f0d541402748849c4c65
Bedig Galladian <bgalladian@gmail.com>
2017-02-27 11:55:33 -0500

    CRUD Functionality for reviews

--- project2/app/controllers/reviews_controller.rb
+++ project2/app/controllers/reviews_controller.rb
@@ -1,10 +1,45 @@
 class ReviewsController < ApplicationController
-  
+
   def index
     @reviews = Review.all
   end
 
   def show
     @review = Review.find(params[:id])
+  end
+
+  def new
+    @movie = Movie.find(params[:movie_id])
+    @review = Review.new
+  end
+
+  def create
+    @movie = Movie.find(params[:movie_id])
+    @review = @movie.reviews.create(review_params)
+    redirect_to movie_review_path(@movie, @review)
+  end
+
+  def edit
+    @movie = Movie.find(params[:movie_id])
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:movie_id])
+    @review = Review.find(params[:id])
+    @review.update(review_params)
+    redirect_to movie_reviews_path(@movie)
+  end
+
+  def destroy
+    @movie = Movie.find(params[:movie_id])
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to movies_path
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:author, :content, :rating)
   end
 end
