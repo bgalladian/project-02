7b0a8fd7cfb26f9bf42ba50b2181edd128a01205
Bedig Galladian <bgalladian@gmail.com>
2017-02-27 09:18:43 -0500

    created db, migrations, and controllers

---
+++ project2/db/migrate/20170227140222_create_movies.rb
@@ -0,0 +1,14 @@
+class CreateMovies < ActiveRecord::Migration[5.0]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.string :director
+      t.text :summary
+      t.string :written_by
+      t.string :poster_url
+      t.datetime :release_date
+
+      t.timestamps
+    end
+  end
+end
