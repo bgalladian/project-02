dbe8afec925deb4f1b441537287ac3ee58fd4542
Bedig Galladian <bgalladian@gmail.com>
2017-02-28 09:57:18 -0500

    added devise functionality

--- project2/app/controllers/movies_controller.rb
+++ project2/app/controllers/movies_controller.rb
@@ -1,4 +1,5 @@
 class MoviesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @movies = Movie.all
--- project2/app/controllers/reviews_controller.rb
+++ project2/app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-
+before_action :authenticate_user!, except: [:index, :show]
   def index
     @movie = Movie.find(params[:movie_id])
     @reviews = Review.all
